a5cf3e122ed7e9ad80330a8e67b566f2feb62966
gabriel.spinola <martinsspn@gmail.com>
2024-12-05 14:57:44 -0300

    #23 endpoint de buscar pedidos do cliente

--- bookstore-backend/bookstore/api/usuario/views.py
+++ bookstore-backend/bookstore/api/usuario/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views import View
+from django.shortcuts import get_object_or_404, render
+from api.usuario.models import Usuario
 
 # Create your views here.
+class ClienteView(View):
+    def buscar_pedidos(self, cliente_id):
+        cliente = get_object_or_404(Usuario, id=cliente_id)
+        cliente_pedidos = [pedido.transform_to_schema() for pedido in cliente.pedidos.all()]
+        return cliente_pedidos
